6380c3a8ba1c602d7bcd921613039a464e079530
Alice <Grayhild@mail.ru>
2024-07-16 01:50:07 +0700

    1

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,15 +2,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from catalog.views import (products_list)
+from catalog.views import (products_list, contacts, home, products_details)
 
 app_name = 'catalog'
 urlpatterns = [
                 path("admin/", admin.site.urls),
                 path('', products_list, name='products_list'),
+                # ! ?
+                path('products/<int:pk>/', products_details, name='products_details'),
+                path('contacts/', contacts),
+                path('home/', home)
+
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 """app_name = CatalogConfig.name
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,32 @@
 from gettext import Catalog
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product, Category
 
 
 # Контроллеры
 
+def contacts(request):
+    return render(request, 'contacts.html')
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
 # Create your views here.
 def products_list(request):
-    #product = Catalog.objects.get(pk=pk)
+    # product = Catalog.objects.get(pk=pk)
+    # !!!
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'products_list.html', context)
+
+
+def products_details(request, pk):
+    # !!!
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product":product}
+    return render(request, 'products_ details.html', context)
+
