ba6d6cc018c742d1ed961ab37722c8a55a3788d4
Alice <Grayhild@mail.ru>
2024-10-28 04:58:48 +0700

    домашняя работа

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,8 @@
 from gettext import Catalog
-
 from django.shortcuts import render, get_object_or_404
-
 from catalog.models import Product, Category
+
+from django.views.generic import ListView, DetailView
 
 
 # Контроллеры
@@ -15,17 +15,11 @@ def home(request):
     return render(request, 'home.html')
 
 
-# Create your views here.
-
-def product_list(request):
-    products = Product.objects.all()
-    # должен быть object_list для прохождение в цикле по всем продуктам
-    context = {"object_list": products}
-    return render(request, "product_list.html", context)
+class ProductListView(ListView):
+    model = Product
+    # catalog/Product_list.html
 
 
-def product_detail(request, pk):
-    # !!!
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, 'product_detail.html', context)
+class ProductDetailView(DetailView):
+    model = Product
+
