42fe223a7dd565a68cfdb8242875ec2e5b2f1ff7
Alice <Grayhild@mail.ru>
2025-01-28 20:28:56 +0700

    неполадки

--- config/settings.py
+++ config/settings.py
@@ -152,14 +152,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-
-"""  EMAIL_HOST_USER = 'angellagrayhild@gmail.com'
-EMAIL_HOST_PASSWORD = ''  """
-
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
 
